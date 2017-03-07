c4bf492b161b7953785f7bd3aea0ae78399541e0
Sahil Shekhawat <sahilshekhawat01@gmail.com>
2017-03-07 22:42:56 +0530

    Removed rocketfuel folder from .gitignore

---
+++ rocketfuel/find.py
@@ -0,0 +1,124 @@
+from __future__ import unicode_literals
+"""
+A Python implementation of RocketFuel topology engine.					
+Copyright (C) 2016 Sahil Shekhawat <sahilshekhawat01@gmail.com> 		
+																		
+This program is free software: you can redistribute it and/or modify	
+it under the terms of the GNU General Public License as published by	
+the Free Software Foundation, either version 3 of the License, org  	
+(at your option) any later version.									
+																		
+This program is distributed in the hope that it will be useful,			
+but WITHOUT ANY WARRANTY; without even the implied warranty of			
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the			
+GNU General Public License for more details.							
+																		
+You should have received a copy of the GNU General Public licenses 		
+along with this program.  If not, see <http://www.gnu.org/licenses/>.	
+
+Type ``rocketfuel license`` to see this message.
+"""
+
+import re
+import os
+import sys
+import random
+import requests
+import ipaddress as ip
+from bs4 import BeautifulSoup
+
+from utils import create_file
+
+def prefix(asn):
+	# ASes = []
+	# f = open('ASes', 'r')
+	# for line in f.readlines():
+	#	 ASes.append(line.rstrip())
+
+	# for asn in ASes:
+	# asn = asn.rstrip('\n')
+	as_base = 'AS'
+	ASN = as_base + asn
+	url_base = 'http://ipinfo.io/'
+	
+	sys.stdout.write("[" + ASN +"] " + "Getting prefixes...")
+	sys.stdout.flush()
+	
+	filename = "Prefix/" + ASN
+	create_file(filename)
+	output = open(filename, "w")
+
+	page = requests.get(url_base+ASN)
+	html_doc = page.content
+	soup = BeautifulSoup(html_doc, 'html.parser')
+	for link in soup.find_all('a'):
+		if asn in link.get('href'):
+			auxstring = '/'+as_base+asn+'/'
+			line = re.sub(auxstring, '', link.get('href'))
+			printstring = line+'\n'
+			if 'AS' not in printstring:
+				output.write(printstring)
+	
+	sys.stdout.write(" Done!\n")
+	sys.stdout.write("[" + ASN +"] " + "Prefixes are in file: ./" + filename + "\n")
+
+def ip_from_prefix(asn):
+	as_base = 'AS'
+	ASN = as_base + asn
+	sys.stdout.write("[" + ASN +"] " + "Getting ip addresses from advertised prefixes...")
+	sys.stdout.flush()
+
+	filename = "IP/" + ASN
+	create_file(filename)
+	output_file = open(os.getcwd() + "/" + filename, 'w')
+	input_file = open(os.getcwd() + "/Prefix/AS" + asn, 'r')
+
+	# Regex taken from http://blog.markhatton.co.uk/2011/03/15/regular-expressions-for-ip-addresses-cidr-ranges-and-hostnames/
+	cidr_regex = re.compile("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\/([0-9]|[1-2][0-9]|3[0-2]))$")
+
+	for line in input_file.readlines():
+
+		is_ipv4_cidr_range = cidr_regex.match(line)
+
+		if is_ipv4_cidr_range:
+			line = line.rstrip('\n')
+			ipnet = ip.ip_network(unicode(line))
+
+			hosts = list(ipnet.hosts())
+			if len(hosts) > 2:
+				output_file.write(str(hosts[random.randint(1, len(hosts)-1)])+"\n")
+				output_file.write(str(hosts[random.randint(1, len(hosts)-1)])+"\n")
+				output_file.write(str(hosts[random.randint(1, len(hosts)-1)])+"\n")
+
+	output_file.close()
+	input_file.close()
+
+	sys.stdout.write(" Done!\n")
+	sys.stdout.write("[" + ASN +"] " + "IPs are in file: ./" + filename + "\n")
+
+def user_slice(api_server, auth, slice_name):
+    """ FIND USER SLICE """
+    print "Finding Nodes in the slice..."
+    try:
+        slice_data = api_server.GetSlices(auth, {}, ['name','node_ids'])
+        given_slice = None
+        for x, y in enumerate(slice_data):
+        	if y.get('name') == slice_name:
+        		given_slice = y
+
+        if given_slice == None:
+        	print "Couldn't find any slice with given name"
+        	sys.exit()
+
+        nodes_ids = given_slice.get('node_ids')
+        node_details = api_server.GetNodes(auth, {'node_id': nodes_ids}, ['hostname'])
+
+    except:
+    	raise
+
+    # if it doesn't goes into except, it goes into else.. :)
+    else:
+    	# extracting only hostname from node_details
+        node_list = [x.get('hostname') for x in node_details]
+        print "Found all Nodes :)"
+        return node_list
---
+++ rocketfuel/rocketfuel.py
@@ -0,0 +1,143 @@
+"""
+A Python implementation of RocketFuel topology engine.					
+Copyright (C) 2016 Sahil Shekhawat <sahilshekhawat01@gmail.com> 		
+																		
+This program is free software: you can redistribute it and/or modify	
+it under the terms of the GNU General Public License as published by	
+the Free Software Foundation, either version 3 of the License, org  	
+(at your option) any later version.									
+																		
+This program is distributed in the hope that it will be useful,			
+but WITHOUT ANY WARRANTY; without even the implied warranty of			
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the			
+GNU General Public License for more details.							
+																		
+You should have received a copy of the GNU General Public licenses 		
+along with this program.  If not, see <http://www.gnu.org/licenses/>.	
+
+Type ``rocketfuel license`` to see this message.
+
+"""
+
+import sys
+import os
+import click
+from xmlrpclib import ServerProxy
+
+import find
+import run
+# from choose_ips_from_prefixes import choose_ips_from_prefixes
+# from find_traceroute_path import find_traceroute_path
+# from run_traceroute import run_traceroute
+
+NODES_DIR = "Nodes" # Directory containing nodes
+MAX_PROCESSES = 50  # No. of processes you want to run.
+
+@click.group()
+def cli():
+	pass
+
+@cli.command()
+def license():
+	"""shows license"""
+	print """
+A Python implementation of RocketFuel topology engine.					
+Copyright (C) 2016 Sahil Shekhawat <sahilshekhawat01@gmail.com> 		
+																		
+This program is free software: you can redistribute it and/or modify	
+it under the terms of the GNU General Public License as published by	
+the Free Software Foundation, either version 3 of the License, org  	
+(at your option) any later version.									
+																		
+This program is distributed in the hope that it will be useful,			
+but WITHOUT ANY WARRANTY; without even the implied warranty of			
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the			
+GNU General Public License for more details.							
+																		
+You should have received a copy of the GNU General Public licenses 		
+along with this program.  If not, see <http://www.gnu.org/licenses/>.	
+
+Type ``rocketfuel license`` to see this message.
+"""
+
+@cli.command()
+@click.argument('AS')
+def analyse(as_number):
+	"""Analyse traceroute output."""
+	# remove AS from the AS number if exists
+	asn = as_number
+	if asn[:2].upper() == "AS":
+		asn = asn[2:]
+
+	print "NOTE: MAKE SURE YOU HAVE ALREADY RUN 'rocketfuel start' OR HAVE TRACEROUTE RESULTS."
+	
+
+@cli.command()
+@click.option('--ases', prompt='ASes file path', help="File containing list of ASes")
+@click.option('--username', prompt='Planet lab account username', help="slice name associated with account")
+@click.option('--password', prompt='Planet lab account password', help="slice name associated with account")
+@click.option('--slicename', prompt='slice name associated with your account', help="slice name associated with account")
+@click.option('--key', prompt='private key file path', help="private key file path to ssh into planet lab nodes.\nVisit https://www.planet-lab.org/ to add you private key")
+def start(ases, username, password, key, slicename):
+
+	if key is None:
+		print "Private key is required to ssh into Planet lab nodes.\n\
+		type 'rocketfuel start --help' for more info"
+	if username is None:
+		print "Username is required to login into Planet lab account.\n\
+		type 'rocketfuel start --help' for more info"
+	if password is None:
+		print "Need password to login into Planet lab account"
+	if slicename is None:
+		print "No slice name passed"
+
+	# getting nodes
+	auth = dict()
+	auth['Username'] = username
+	auth['AuthString'] = password
+	auth['AuthMethod'] = "password"
+
+	try:
+		api_server = ServerProxy('https://www.planet-lab.org/PLCAPI/')
+		if run.planet_lab_auth(api_server, auth):
+			node_list = find.user_slice(api_server, auth, slicename)
+
+			if not os.path.exists(NODES_DIR):
+				os.mkdir(NODES_DIR)
+
+			# Separating nodes into 50 parts which will run concurrently
+			node_index = 0
+			node_limit = len(node_list)/MAX_PROCESSES
+			for i in xrange(0, MAX_PROCESSES):
+				fnodes = open(NODES_DIR + "/nodes_" + str(i), 'w')
+				nodes_added = 0
+				while nodes_added < node_limit:
+					fnodes.write(node_list[node_index] + "\n")
+					node_index += 1
+					nodes_added += 1
+				fnodes.close()
+
+		else:
+			sys.exit()
+	except KeyboardInterrupt:
+		sys.exit()
+	except:
+		raise
+
+	"""start traceroutes to ASes from Planet Lab nodes."""
+	# remove AS from the AS number if exists
+
+	f = open(ases, 'r')
+	for line in f.readlines():
+		line = line.rstrip()
+		asn = line
+
+		if asn[:2].upper() == "AS":
+			asn = asn[2:]
+
+		#Get all advertised prefixes for the AS.
+		find.prefix(asn)
+		#Randomly get some ips from each advertised prefixes.
+		find.ip_from_prefix(asn)
+		#Finally, run the traceroute
+		run.traceroute(asn, key, slicename)
